7f09349e2cf8512d58a3122d241dea574e43381a
samuelcpr <samuelcpr464@gmail.com>
2022-08-04 09:18:26 -0300

    chore: create mock

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,8 +2,14 @@ import { MissingParamError } from "../erros/missing-param-error";
 import { HttpResponse, HttpRequest } from "../protocools/http";
 import { badRequest } from "../helpers/http-helpers";
 import { Controller } from "../protocools/controller";
+import { EmailValidator } from "../protocools/emailValidator";
+import { InvalidParamError } from "../erros/invalid-param-error";
 
 export class SignUpController implements Controller {
+	private readonly emailValidator: EmailValidator;
+	constructor(emailValidator: EmailValidator) {
+		this.emailValidator = emailValidator;
+	}
 	handle(httpRequest: HttpRequest): HttpResponse {
 		const requiredFields = [
 			"name",
@@ -15,6 +21,10 @@ export class SignUpController implements Controller {
 			if (!httpRequest.body[field]) {
 				return badRequest(new MissingParamError(field));
 			}
+		}
+		const isValid = this.emailValidator.isValid(httpRequest.body.email);
+		if (!isValid) {
+			return badRequest(new InvalidParamError("email"));
 		}
 	}
 }
