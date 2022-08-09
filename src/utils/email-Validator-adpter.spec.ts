b5ff092b8e0f48fce7dab325544e9e9c93a22003
samuelcpr <samuelcpr464@gmail.com>
2022-08-08 23:45:43 -0300

    refactor: move sut  creation to a fectory helper method

--- src/utils/email-Validator-adpter.spec.ts
+++ src/utils/email-Validator-adpter.spec.ts
@@ -7,20 +7,24 @@ jest.mock("validator", () => ({
 	},
 }));
 
+const makeSut = (): EmailValidatorAdapter => {
+	return new EmailValidatorAdapter();
+};
+
 describe("EmailValiadtor Adapter", () => {
 	test("shold return false if validator retuns false", () => {
-		const sut = new EmailValidatorAdapter();
+		const sut = makeSut();
 		jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
 		const isValid = sut.isValid("invalid_email@mail.com");
 		expect(isValid).toBe(false);
 	});
 	test("shold return true if validator retuns true", () => {
-		const sut = new EmailValidatorAdapter();
+		const sut = makeSut();
 		const isValid = sut.isValid("valid_email@mail.com");
 		expect(isValid).toBe(true);
 	});
 	test("shold return true if validator retuns true", () => {
-		const sut = new EmailValidatorAdapter();
+		const sut = makeSut();
 		const isEmailSpy = jest.spyOn(validator, "isEmail");
 		sut.isValid("any_email@mail.com");
 		expect(isEmailSpy).toHaveBeenCalledWith("any_email@mail.com");
